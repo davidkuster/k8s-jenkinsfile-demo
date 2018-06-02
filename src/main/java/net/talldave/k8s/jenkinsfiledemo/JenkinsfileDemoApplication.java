4c1e172d4e382df6a17542be8542ed40c2427de1
David Kuster <dave@talldave.net>
2018-06-02 14:45:59 -0500

    initial app skeleton from start.spring.io

---
+++ src/main/java/net/talldave/k8s/jenkinsfiledemo/JenkinsfileDemoApplication.java
@@ -0,0 +1,12 @@
+package net.talldave.k8s.jenkinsfiledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsfileDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsfileDemoApplication.class, args);
+	}
+}
